e74ad567ee15823597b8b60abb6ceb11be6a5be6
rohitb <rbhogal23@gmail.com>
2021-10-06 18:05:25 -0700

    progress made on project pages mostly burgerlords

--- src/js/script.js
+++ src/js/script.js
@@ -1,80 +1,83 @@
 /* 
-//Loading screen
-const loadingScreen = document.querySelector(".loading-screen");
-const loadingScreenAddition = document.querySelector(
-  ".loading-screen-addition"
-);
-const loadingIntro = document.querySelector(".loading-intro");
-const loadingFirstName = document.querySelector(
-  ".loading-intro__text--first-name"
-);
-const loadingLastName = document.querySelector(
-  ".loading-intro__text--last-name"
-);
- 
-//Body
-const bodyContainer = document.querySelector(".container");
-//Header
-const header = document.querySelector(".header");
-const aboutLink = document.querySelector(".navbar-item__about");
+    //Loading screen
+    const loadingScreen = document.querySelector(".loading-screen");
+    const loadingScreenAddition = document.querySelector(
+      ".loading-screen-addition"
+    );
+    const loadingIntro = document.querySelector(".loading-intro");
+    const loadingFirstName = document.querySelector(
+      ".loading-intro__text--first-name"
+    );
+    const loadingLastName = document.querySelector(
+      ".loading-intro__text--last-name"
+    );
 
-//Footer
-const footer = document.querySelector(".footer");
+    //Body
+    const bodyContainer = document.querySelector(".container");
+    //Header
+    const header = document.querySelector(".header");
+    const aboutLink = document.querySelector(".navbar-item__about");
 
-//Home
-const projectsBtn = document.querySelector(".projects__btn");
+    //Footer
+    const footer = document.querySelector(".footer");
 
-//Projects
-const projectsNumber02 = document.querySelector(".number__02");
-const projectsHeading = document.querySelector(".projects__heading");
-const projectsCard = document.querySelectorAll(".projects__card");
+    //Home
+    // const projectsBtn = document.querySelector(".projects__btn");
 
-// Loading Text (Name) - Enter
-const loadingScreenTL = gsap.timeline();
+    //Projects
+    // const projectsNumber02 = document.querySelector(".number__02");
+    // const projectsHeading = document.querySelector(".projects__heading");
+    // const projectsCard = document.querySelectorAll(".projects__card");
 
-gsap.from(loadingFirstName, {
-  duration: 1.5,
-  y: 25,
-  ease: Expo.easeInOut,
-});
+    // Loading Text (Name) - Enter
+    const loadingScreenTL = gsap.timeline();
 
-gsap.from(loadingLastName, {
-  duration: 1.5,
-  y: -25,
-  ease: Expo.easeInOut,
-});
+    gsap.from(loadingFirstName, {
+      duration: 1.5,
+      y: 26,
+      ease: Expo.easeInOut,
+    });
 
-// Loading Text (Name) - Exit
-const loadingNameTL = gsap.timeline();
+    gsap.from(loadingLastName, {
+      duration: 1.5,
+      y: -25,
+      ease: Expo.easeInOut,
+    });
 
-loadingNameTL.to([loadingFirstName, loadingLastName], {
-  duration: 1,
-  delay: 1.5,
-  ease: Expo.easeInOut,
-  y: -25,
-});
+    // Loading Text (Name) - Exit
+    const loadingNameTL = gsap.timeline();
 
-// Loading Screen Exit
-loadingScreenTL.to([loadingScreen, loadingIntro], {
-  duration: 1.5,
-  delay: 1.5,
-  top: "-110%",
-  ease: Expo.easeInOut,
-}).from([projectsNumber02, projectsHeading], {
-  opacity: 0,
-  delay: -0.7,
-  y: 110,
-  duration: 1.5,
-  ease: "power3.out",
-  stagger: 0.2,
-});;
+    loadingNameTL.to([loadingFirstName, loadingLastName], {
+      duration: 1,
+      delay: 1.5,
+      ease: Expo.easeInOut,
+      y: -25,
+    });
 
-gsap.to(bodyContainer, {
-  duration: 0,
-  delay: 2.3,
-  overflow: "visible",
-});
- 
+    // Loading Screen Exit
+    loadingScreenTL.to([loadingScreen, loadingIntro], {
+      duration: 1.5,
+      delay: 1.5,
+      top: "-110%",
+      ease: Expo.easeInOut,
+    });
+    // .from([projectsNumber02, projectsHeading], {
+    //   opacity: 0,
+    //   delay: -0.7,
+    //   y: 110,
+    //   duration: 1.5,
+    //   ease: "power3.out",
+    //   stagger: 0.2,
+    // });
+
+    gsap.to(bodyContainer, {
+      duration: 0,
+      delay: 2.3,
+      overflow: "visible",
+    });
+   */
+
+/*
 //Projects Section
 const projectsHeaderTL = gsap.timeline();
 
@@ -89,7 +92,7 @@ projectCardsTL.from(projectsCard, {
   ease: "power3.out",
   stagger: 0.2,
 });
-*/
+ */
 const controller = new ScrollMagic.Controller();
 /* 
 new ScrollMagic.Scene({
