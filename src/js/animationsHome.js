770410dcd315a258c087f1a0aa525f175eb2784d
rohitb <rbhogal23@gmail.com>
2021-10-26 13:46:36 -0700

    changed loading screen

--- src/js/animationsHome.js
+++ src/js/animationsHome.js
@@ -105,7 +105,7 @@ if (window.innerWidth > 1200) {
     .to(loadingScreen, {
       duration: 0.75,
       delay: 0.75,
-      top: "-100%",
+      top: "-110%",
       ease: Expo.easeInOut,
     })
     .from([header, navLink], {
