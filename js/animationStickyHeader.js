271aa71985e84e1665ca233ecbda5ab182c6a2f4
rohitb <rbhogal23@gmail.com>
2022-03-04 22:00:48 -0800

    added get fit app project

---
+++ js/animationStickyHeader.js
@@ -0,0 +1,19 @@
+// window.onscroll = function () {
+//   stickyHeader();
+// };
+
+// const header = document.querySelector(".home-header__nav-wrapper");
+// const projects = document.querySelector(".projects");
+
+// // Get the offset position of the header
+// const sticky = header.offsetTop;
+
+// function stickyHeader() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//     projects.classList.add("projects-sticky-padding");
+//   } else {
+//     header.classList.remove("sticky");
+//     projects.classList.remove("projects-sticky-padding");
+//   }
+// }
